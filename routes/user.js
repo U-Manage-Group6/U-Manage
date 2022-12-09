ba4c79703b25e5d0cdd36f796b5b82b8698f9747
Daniel Aguilera <dan.aguilera97@gmail.com>
2022-12-08 21:56:59 -0600

    small tweaks within the user.js file

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,4 @@
-const { query } = require('express');
 const express = require('express');
-const { restoreDefaultPrompts } = require('inquirer');
 const connection = require('../connection');
 const router = express.Router();
 
@@ -10,10 +8,18 @@ router.post('/signup', (req,res)=>{
     connection.query(query,[user.email],(err,result) =>
     {if(!err){
         if(results.length <=0){
-            query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?, 'false')"
+            query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?, 'false', 'user')"
+            connection.query(query,[user.name,user.contactNumber,user.email,user.password],(err,results) => 
+            {if(!err){
+                return res.status(200).json({message:"Successfully Registered"});
+            }
+            else{
+                return res.status(400).json({message: "Email Already Exist"})
+            }
+            }) 
         }
         else{
-            return res.status(400).json({message: 'Eamil Already Exists. '})
+            return res.status(400).json({message: 'Email Already Exists. '})
         }
     }
     else {
